c0acf5594daf4a36f37349f578500390e18da935
Isaac <git@eartharoid.me>
2021-11-04 16:26:10 +0000

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 			'redocusaurus',
 			{
 				specs: [{
-					specUrl: 'https://cdn.statically.io/gh/christmas-countdown/api/main/openapi.yaml',
+					specUrl: 'https://cdn.statically.io/gh/christmas-countdown/api/main/swagger/openapi.json',
 					routePath: 'api/pine'
 				}],
 			}
